013964b2f8d029f3f0ac9ac2a8967d1cc46851e5
Junjie <git@junjie.im>
2021-06-01 18:38:52 +0800

    命令行参数解析的错误处理

--- rust programming language/char12/minigrep/src/main.rs
+++ rust programming language/char12/minigrep/src/main.rs
@@ -1,14 +1,21 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args).expect("aaa ");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {} in file {}", config.query, config.filename);
 
     let content = fs::read_to_string(config.filename)
         .expect("Something wrong reading the file.");
 
+    println!("Content in file:");
     println!("{}", content);
 }
 

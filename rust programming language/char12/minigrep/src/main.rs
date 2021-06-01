c4317947756466b9817eadf976bb594624cf4d5d
Junjie <git@junjie.im>
2021-06-01 16:54:44 +0800

    关注分离

--- rust programming language/char12/minigrep/src/main.rs
+++ rust programming language/char12/minigrep/src/main.rs
@@ -1,14 +1,28 @@
 use std::env;
 use std::fs;
 
+#[warn(unused_variables)]
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    // let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    let content = fs::read_to_string(filename)
+    let content = fs::read_to_string(config.filename)
         .expect("Something wrong reading the file.");
 
     println!("{}", content);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
 }

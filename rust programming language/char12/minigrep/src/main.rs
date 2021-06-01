aabf31309a586db0a64572bd8843419824dd656e
Junjie <git@junjie.im>
2021-06-01 17:37:31 +0800

    改进Config结构体的返回值为Result

--- rust programming language/char12/minigrep/src/main.rs
+++ rust programming language/char12/minigrep/src/main.rs
@@ -1,12 +1,10 @@
 use std::env;
 use std::fs;
 
-#[warn(unused_variables)]
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::new(&args).expect("aaa ");
 
     let content = fs::read_to_string(config.filename)
         .expect("Something wrong reading the file.");
@@ -14,15 +12,20 @@ fn main() {
     println!("{}", content);
 }
 
+#[allow(dead_code)]
 struct Config {
     query: String,
     filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
 }

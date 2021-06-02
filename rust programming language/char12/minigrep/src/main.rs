3692a1265cb7bb0614c0dd7997d8ef940252aa7a
Junjie <git@junjie.im>
2021-06-02 09:48:22 +0800

    拆分到 lib.rs

--- rust programming language/char12/minigrep/src/main.rs
+++ rust programming language/char12/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,34 +13,10 @@ fn main() {
 
     println!("Searching for {} in file {}", config.query, config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Problem parsing arguments: {}", e);
         process::exit(1);
     };
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config {query, filename})
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(&config.filename)?;
-
-    println!("Content in {}:", config.filename);
-    println!("{}", content);
-
-    Ok(())
-}

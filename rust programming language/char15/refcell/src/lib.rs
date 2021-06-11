d4a91cbec18840474a5d9cc8f2addcfedd707b62
Junjie <git@junjie.im>
2021-06-11 16:31:27 +0800

    refcell

---
+++ rust programming language/char15/refcell/src/lib.rs
@@ -0,0 +1,66 @@
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T: Messenger> LimitTracker<'a, T> {
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger.send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {sent_messages: RefCell::new(vec![]),}
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, messenger: &str) {
+            self.sent_messages.borrow_mut().push(String::from(messenger));
+        }
+    }
+
+    #[test]
+    fn it_works() {
+        let mock = MockMessenger::new();
+
+        let mut tracker = LimitTracker::new(&mock, 100);
+
+        tracker.set_value(80);
+
+        assert_eq!(mock.sent_messages.borrow().len(), 1);
+    }
+}

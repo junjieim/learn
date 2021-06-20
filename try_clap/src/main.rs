e8f6f53e59aad86b1707223137a7140d63f8dc90
Junjie <git@junjie.im>
2021-06-20 14:23:17 +0800

    try clap crate

---
+++ try_clap/src/main.rs
@@ -0,0 +1,20 @@
+use clap::clap_app;
+
+fn main() {
+    let matches = clap_app!(myapp =>
+        (version: "1.0")
+        (author: "Kevin K. <kbknapp@gmail.com>")
+        (about: "Does awesome things")
+        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+        (@arg INPUT: +required "Sets the input file to use")
+        (@arg verbose: -v --verbose "Print test information verbosely")
+        (@subcommand test =>
+            (about: "controls testing features")
+            (version: "1.3")
+            (author: "Someone E. <someone_else@other.com>")
+            (@arg debug: -d ... "Sets the level of debugging information")
+        )
+    ).get_matches();
+
+    // Same as previous examples...
+}
